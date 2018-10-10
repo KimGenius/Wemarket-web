73aabb223c363d52d13a87ce42b973915cf9e59a
GeniusK <geniusk1047@naver.com>
2018-10-11 01:36:57 +0900

    waitjoin 작업

--- src/views/WaitJoin/WaitJoin.vue
+++ src/views/WaitJoin/WaitJoin.vue
@@ -1,23 +1,20 @@
 <template>
-    <div class="main">
-        <app-intro :app-name=appName />
-        <div class="wait-join-wrap">
-            <p class="wait-join-text">해당 계정은 가입 승인 대기중입니다</p>
-            <a class="wait-join-logout" href="#">다른 계정으로 로그인 해 볼까요? 로그아웃</a>
+    <div class='main'>
+        <app-intro app-name='위마켓' />
+        <div class='wait-join-wrap'>
+            <p class='wait-join-text'>해당 계정은 가입 승인 대기중입니다</p>
+            <router-link to="/login" class='wait-join-logout'>다른 계정으로 로그인 해 볼까요? 로그아웃</router-link>
         </div>
     </div>
 </template>
 
 <script>
-  import AppIntro from "../../components/appIntro";
+  import AppIntro from '../../components/appIntro';
   import './waitJoin.scss';
 
   export default {
     name: 'Login',
     components: {AppIntro},
-    props: {
-      appName: String
-    },
     data: () => ({
     })
   }
