13bf1d398a6f6b3484f19ec30e8a7c09a4e97af8
GeniusK <geniusk1047@naver.com>
2018-10-12 00:35:08 +0900

    로그인 화면 수정중 (회원가입 버튼만들어야됨)

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,22 +1,25 @@
 <template>
     <div class='main'>
-        <app-intro app-name='위마켓' />
+        <app-intro app-title='We<br>Market<br>당신을<br>위한' />
         <div class='login-wrap'>
             <v-form v-model='valid'>
                 <v-flex xs12 sm6 md3>
                     <v-text-field
+                            hide-details=true
                             class='login-input'
                             label='Solo'
                             placeholder='아이디'
                             solo
                     ></v-text-field>
                     <v-text-field
+                            hide-details=true
                             class='login-input'
                             label='Solo'
                             placeholder='패스워드'
                             solo
                     ></v-text-field>
                     <v-btn class='login-input login-submit'>로그인</v-btn>
+                    <!-- TODO : 회원가입 작업 해야됨-->
                     <router-link to="/waitjoin" class='login-join'>아직 계정이 없으신가요? 회원가입</router-link>
                 </v-flex>
             </v-form>
