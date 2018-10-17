d63957dac37f260b252f2d209027ab1113a11067
GeniusK <geniusk1047@naver.com>
2018-10-18 02:33:40 +0900

    파일 이름 변경 및 구조 변경

---
+++ src/components/AppIntro/appIntro.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="app-intro" :style='style'>
+        <img src="../../src/assets/logo.png" alt="logo" class="app-logo"/>
+        <p class="app-text" v-html="appTitle"></p>
+    </div>
+</template>
+
+<script>
+  export default {
+    name: "appIntro",
+    props: {
+      appTitle: String,
+      backgroundColor: String
+    },
+    computed: {
+      style () {
+        return 'background-color: ' + this.backgroundColor
+      }
+    }
+  }
+</script>
+
+<style scoped>
+</style>
