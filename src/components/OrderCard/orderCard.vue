489d3cd1166acce536faa78ff065be7028dfabab
GeniusK <geniusk1047@naver.com>
2018-11-10 06:13:42 +0900

    orderCard 작업 및 적용

--- src/components/OrderCard/orderCard.vue
+++ src/components/OrderCard/orderCard.vue
@@ -1,6 +1,17 @@
 <template>
-    <div>
-        orderCard
+    <div class="order-card-wrap">
+        <div class="order-card-type">
+            <p>카카오페이</p>
+        </div>
+        <div class="order-card-info">
+            <p>
+                큐브스테이크 2개<br>
+                치즈큐브스테이크 1개
+            </p>
+            <p class="order-card-info-date">18.11.03 8:12</p>
+            <p class="order-card-info-price">31,500₩</p>
+            <p class="order-card-info-finish">완료하기</p>
+        </div>
     </div>
 </template>
 
--- src/components/OrderList/orderList.vue
+++ src/components/OrderList/orderList.vue
@@ -1,6 +1,7 @@
 <template>
     <div>
         <OrderCard></OrderCard>
+        <OrderCard></OrderCard>
     </div>
 </template>
 
