3e47e6c2531f9ede74e26290383020269917bdb7
GeniusK <geniusk1047@naver.com>
2018-11-12 07:08:14 +0900

    QR 데이터 연동

--- src/components/SellerQR/sellerQR.vue
+++ src/components/SellerQR/sellerQR.vue
@@ -1,13 +1,13 @@
 <template>
     <div class="seller-qr-wrap" :style="onQR">
-        <img src="../../assets/qr@3x.png" alt="qr">
+        <img src="../../assets/websiteQRCode_noFrame.png" alt="qr">
         <p class="seller-qr-name">{{storeName}}</p>
         <p class="seller-qr-phone">{{phone}}</p>
         <p class="seller-qr-content">이제 위 QR코드를 스캔하여<br>
             상품을 주문할 수 있습니다</p>
         <p class="seller-qr-link">
             혹은 URL로 접속<br>
-            <a href="http://www.westore.co.kr/westore">www.westore.co.kr/westore</a>
+            <a :href="this.url">{{this.url}}</a>
         </p>
     </div>
 </template>
@@ -34,9 +34,11 @@
     created() {
       // Using the service bus
       const cookieToken = cookie.get('WMUD')
-      const {storeName, phone} = jwt.decode(cookieToken)
+      const {idx, storeName, phone} = jwt.decode(cookieToken)
       this.storeName = storeName
       this.phone = phone
+      this.idx = idx
+      this.url = `http://www.wemarket.kr/customer?sellerId=${idx}`
       serverBus.$on('toggleSellerQR', () => {
         this.isSellerQR = !this.isSellerQR;
       });
