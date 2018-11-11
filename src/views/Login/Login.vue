8f7ddc78bf4b19a272d878315997e9fb27f252c1
GeniusK <geniusk1047@naver.com>
2018-11-12 00:13:06 +0900

    login 연동

--- src/views/Login/Login.vue
+++ src/views/Login/Login.vue
@@ -1,14 +1,15 @@
 <template>
     <div class='main'>
-        <app-intro app-title='We<br>Market<br>당신을<br>위한' app-color="#ffa945" :logo-color="color" />
+        <app-intro app-title='We<br>Market<br>당신을<br>위한' app-color="#ffa945" :logo-color="color"/>
         <div class='login-wrap'>
-            <v-form v-model='valid'>
+            <v-form>
                 <v-flex xs12 sm6 md3>
                     <v-text-field
                             :hideDetails=true
                             class='login-input'
                             label='아이디'
                             color='#ff9922'
+                            v-model="loginData.id"
                             box
                     ></v-text-field>
                     <v-text-field
@@ -16,9 +17,10 @@
                             class='login-input'
                             label='패스워드'
                             solo
+                            v-model="loginData.pw"
                             type='password'
                     ></v-text-field>
-                    <v-btn class='login-input login-submit'>로그인</v-btn>
+                    <v-btn @click="submitLogin" class='login-input login-submit'>로그인</v-btn>
                 </v-flex>
             </v-form>
         </div>
@@ -32,27 +34,43 @@
   import AppIntro from '../../components/AppIntro'
   import Join from '../../components/Join'
   import './login.scss'
+  import axios from 'axios'
+  import jwt from 'jsonwebtoken'
+  import cookie from 'js-cookie'
 
   export default {
     name: 'Login',
     components: {AppIntro, Join},
     data: () => ({
-      valid: false,
-      name: '',
-      nameRules: [
-        v => !!v || 'Name is required',
-        v => v.length <= 10 || 'Name must be less than 10 characters'
-      ],
-      email: '',
-      emailRules: [
-        v => !!v || 'E-mail is required',
-        v => /.+@.+/.test(v) || 'E-mail must be valid'
-      ],
+      loginData: {
+        id: '',
+        pw: ''
+      },
       color: ''
     }),
     methods: {
       changeColor: function () {
         this.color = this.color === '' ? 'rgba(0, 0, 0, .6)' : ''
+      },
+      async submitLogin() {
+        const {loginData} = this
+        try {
+          const {data} = await axios.post('http://localhost:3000/login', {
+            id: loginData.id,
+            pw: loginData.pw
+          })
+          const token = jwt.sign(data, 'shhhhh')
+          cookie.set('WMUD', token)
+          alert('환영합니다')
+          this.$router.replace('/seller')
+          // const cookieToken = cookie.get('WMUD', token)
+          // const decoded = jwt.decode(cookieToken)
+          // console.log(decoded)
+        } catch (e) {
+          if (e.message === 'Request failed with status code 404'){
+            alert('존재하지 않는 계정입니다.')
+          }
+        }
       }
     }
   }
