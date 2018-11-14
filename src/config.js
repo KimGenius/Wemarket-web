2b279f77af1d789f448d3823d73b28623ca564f2
GeniusK <geniusk1047@naver.com>
2018-11-15 01:37:03 +0900

    config 수정

--- src/config.js
+++ src/config.js
@@ -1,3 +1,13 @@
-module.exports = {
-  host: 'http://192.168.101.62:3000'
+import jwt from 'jsonwebtoken'
+import cookie from 'js-cookie'
+
+export const host = 'http://localhost:3000'
+export const getCookie = () => {
+  const cookieToken = cookie.get('WMUD')
+  return jwt.decode(cookieToken)
 }
+const config = {
+  host,
+  getCookie
+}
+export default config
