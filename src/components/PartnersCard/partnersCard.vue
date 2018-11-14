a2c7a5bc863f55d2a32eae6f80328a569c7bbfa8
GeniusK <geniusk1047@naver.com>
2018-11-15 04:26:49 +0900

    신청 유무 처리

--- src/components/PartnersCard/partnersCard.vue
+++ src/components/PartnersCard/partnersCard.vue
@@ -4,18 +4,42 @@
         <p>{{item.endDate}} 마감</p>
         <p>{{item.content}}</p>
         <div>
-            <p>신청하기</p>
+            <p :style="isSubmitStyle">신청하기</p>
+            <p :style="isNotSubmitStyle">신청됨</p>
         </div>
     </div>
 </template>
 
 <script>
   import './partnersCard.scss'
+  import config from '../../config'
 
   export default {
     name: "partnersCard",
     props: {
       item: Object
+    },
+    data: () => ({
+      isSubmit: false
+    }),
+    computed: {
+      isSubmitStyle() {
+        return {
+          display: this.isSubmit ? 'block' : 'none'
+        }
+      },
+      isNotSubmitStyle() {
+        return {
+          display: this.isSubmit ? 'none' : 'block'
+        }
+      }
+    },
+    beforeMount() {
+      this.item.udx.split(',').map(u => {
+        if (u == config.getCookie().idx) {
+          this.isSubmit = true
+        }
+      })
     }
   }
 </script>
