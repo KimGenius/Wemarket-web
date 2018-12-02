48e40c960095d171d9e1166450b04b65b9450244
GeniusK <geniusk1047@naver.com>
2018-12-02 23:51:14 +0900

    커스터머쪽 가게정보에 소개 문제 해결

--- src/components/CustomerContentIntroBox/customerContentIntroBox.vue
+++ src/components/CustomerContentIntroBox/customerContentIntroBox.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="customer-content-intro-box-wrap">
         <p id="storePhone">{{phone}}</p>
-        <textarea style="resize: none;" rows="3" :readonly="true" id="storeDesc" v-model="storeDesc"></textarea>
+        <textarea style="resize: none; width: 90%" rows="3" :readonly="true" id="storeDesc" v-model="storeDesc"></textarea>
     </div>
 </template>
 
